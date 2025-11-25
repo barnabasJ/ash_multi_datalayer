c22dfb987ef7e834ba779f21b9a79a4ca5e059b0
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2025-11-25 10:36:14 +0100

    inital commit

---
+++ lib/ash_multi_datalayer.ex
@@ -0,0 +1,18 @@
+defmodule AshMultiDatalayer do
+  @moduledoc """
+  Documentation for `AshMultiDatalayer`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AshMultiDatalayer.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ash_multi_datalayer_test.exs
@@ -0,0 +1,8 @@
+defmodule AshMultiDatalayerTest do
+  use ExUnit.Case
+  doctest AshMultiDatalayer
+
+  test "greets the world" do
+    assert AshMultiDatalayer.hello() == :world
+  end
+end
